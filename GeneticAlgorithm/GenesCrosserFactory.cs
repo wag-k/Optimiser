66e5ddbd9593e3e4fb540641e358f18c658c5148
wag_k <captenkenta@gmail.com>
2022-01-23 10:32:23 +0900

    feat: add GenesCrosserFactory

---
+++ GeneticAlgorithm/GenesCrosserFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Algorithm;
+
+namespace Optimiser.GeneticAlgorithm
+{
+    /// <summary>
+    /// enum of cross over method option
+    /// </summary>
+    public enum CrossOverMethod{
+        OnePoint = 0,
+        TwoPoints,
+        Uniform,
+    }
+
+    /// <summary>
+    /// GenesCrosserFactory
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class GenesCrosserFactory<T>
+    {
+        /// <summary>
+        /// delegate of Crossing over genes pair method
+        /// </summary>
+        /// <param name="chromosomes"> an ensenble of chromosomes</param>
+        /// <returns></returns>
+        public delegate T[][] CrossOver(T[][] originalGenesPair);
+
+        static Random Rand { get; set; }
+
+
+        public static CrossOver Create(CrossOverMethod selection){
+            switch (selection)
+            {
+                case CrossOverMethod.OnePoint: 
+                case CrossOverMethod.TwoPoints:
+                case CrossOverMethod.Uniform: 
+                default:
+                    return null;
+            }
+        }
+        
+        static GenesCrosserFactory(){
+            Rand = new Random(DateTime.Now.GetHashCode());
+        }
+
+    }
+}
