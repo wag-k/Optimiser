7a702b9f45926b62c371dc9123d9f450c4389693
wag_k <captenkenta@gmail.com>
2022-01-25 00:50:31 +0900

    feat: add CrossOverInTwoPoints

--- GeneticAlgorithm/GenesCrosserFactory.cs
+++ GeneticAlgorithm/GenesCrosserFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using Algorithm;
@@ -46,6 +47,35 @@ namespace Optimiser.GeneticAlgorithm
             Rand = new Random(DateTime.Now.GetHashCode());
         }
 
-        
+        /// <summary>
+        /// CrossOver Genes in two points
+        /// </summary>
+        /// <param name="originalGenesPair"></param>
+        /// <returns>crossed genes pair</returns>
+        public static T[][] CrossOverInTwoPoints(T[][] originalGenesPair){
+            if (originalGenesPair.Length != 2){
+                throw new ArgumentException($"{nameof(CrossOverInTwoPoints)}Error: originalGenesPair Length must be 2");
+            }
+            var locusPair = GeneManipulationUtility.GetRandomLocusPair(originalGenesPair.Length);
+            var startLocus = locusPair[0];
+            var endLocus = locusPair[1];
+            var crossedLength = endLocus - startLocus;
+            var crossedGenesArea1 = 
+                (from locus in Enumerable.Range(startLocus, crossedLength)
+                    select originalGenesPair[0][locus]
+                ).ToArray();
+            var crossedGenes1 = new T[originalGenesPair[0].Length];
+            Array.Copy(originalGenesPair[0], crossedGenes1, crossedGenes1.Length);
+            var crossedGenes2 = new T[originalGenesPair[1].Length];
+            Array.Copy(originalGenesPair[1], crossedGenes2, crossedGenes2.Length);
+
+            for (int index = 0; index < crossedLength; ++ index){
+                crossedGenes1[startLocus + index] = crossedGenes2[startLocus + index];
+                crossedGenes2[startLocus + index] = crossedGenesArea1[index];
+            }
+
+            return new T[2][]{crossedGenes1, crossedGenes2};
+        }
+
     }
 }
