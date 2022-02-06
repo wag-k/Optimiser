64d62adc5148059cbfab6dfb49394bb2260cae03
wag_k <captenkenta@gmail.com>
2022-02-06 10:13:52 +0900

    feat: add mutate inversion

--- GeneticAlgorithm/GenesMutatorFactory.cs
+++ GeneticAlgorithm/GenesMutatorFactory.cs
@@ -53,5 +53,26 @@ namespace Optimiser.GeneticAlgorithm
             targetChromosome[locus] = targetChromosome.GetAllele(targetChromosome[locus]);
         }
 
+        /// <summary>
+        /// Chromosomal inversion
+        /// Inverting the order of genes between two genes selected Randomly 
+        /// </summary>
+        /// <param name="targetChromsome"></param>
+        private static void MutateInversion(ref IChromosome<T> targetChromsome){
+            var selectedPair = GeneManipulationUtility.GetRandomLocusPair(targetChromsome.Length);
+            var startLocus = selectedPair[0];
+            var partialGenesLength = selectedPair[1] - selectedPair[0] +1;
+            var partialGenes = new T[partialGenesLength];
+            Array.Copy(targetChromsome.Genes, startLocus, partialGenes, 0, partialGenesLength);
+
+            /// <summary>
+            /// Invert
+            /// </summary>
+            /// <value></value>
+            for (int locus = 0; locus < partialGenesLength; ++locus){
+                targetChromsome[startLocus + locus] = partialGenes[partialGenesLength - 1 - locus];
+            }
+        }
+
     }
 }
