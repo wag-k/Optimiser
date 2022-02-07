814ac717a8d92175f206c65a3455e147e309c20c
wag_k <captenkenta@gmail.com>
2022-02-07 23:00:10 +0900

    feat: Select, CrossOver, Mutate in GA

--- GeneticAlgorithm/GA.cs
+++ GeneticAlgorithm/GA.cs
@@ -9,5 +9,29 @@ namespace Optimiser.GeneticAlgorithm
     [DataContract]
     public class GA<T>
     {
+        ChromosomeEnthumble<T> CurrentEnthumble { get; set;}
+
+        /**
+        public IEnumerable<T[]> GeneticManipulation(IList<IChromosome<T> enthumble){
+            
+        }
+        */
+
+        public static int[] Select(IList<IChromosome<T>> chromosomes, Selection selection){
+            var select = GenesSelectorFactory<T>.Create(selection);
+            var selectedGenesPair = select(chromosomes);
+            return selectedGenesPair;
+        } 
+
+        public static T[][] CrossOver(T[][] originalGenesPair, CrossOverMethod crossOverMethod){
+            var crossOver = GenesCrosserFactory<T>.Create(crossOverMethod);
+            return crossOver(originalGenesPair);
+        }
+
+        public static void Mutate(ref IChromosome<T> chromosome, Mutation mutation){
+            var mutate = GenesMutatorFactory<T>.Create(mutation);
+            mutate(ref chromosome);
+        }
+
     }
 }
