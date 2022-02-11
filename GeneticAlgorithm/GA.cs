64532426bedfdfbe6fb35ca5195eeafc9784359b
wag_k <captenkenta@gmail.com>
2022-02-11 21:23:15 +0900

    feat: GeneticManipulation

--- GeneticAlgorithm/GA.cs
+++ GeneticAlgorithm/GA.cs
@@ -12,8 +12,45 @@ namespace Optimiser.GeneticAlgorithm
         public Selection Selection {get; set;}
         public CrossOverMethod CrossOverMethod {get; set;}
 
-        public int MutationRate{ get; set; }
+        public double SingleMutationRate{ 
+            get {return singleMutationRate;}
+            set {
+                if(0 <= singleMutationRate && singleMutationRate <= 1){
+                    singleMutationRate = value;
+                }else{
+                    throw new ArgumentException($"{nameof(SingleMutationRate)} must be 0 <= rate <= 1");
+                }
+            }
+        }
+        double singleMutationRate;
+        public double InversingMutationRate{ 
+            get {return inversingMutationRate;}
+            set {
+                if(0 <= inversingMutationRate && inversingMutationRate <= 1){
+                    inversingMutationRate = value;
+                }else{
+                    throw new ArgumentException($"{nameof(SingleMutationRate)} must be 0 <= rate <= 1");
+                }
+            }
+        }
+        double inversingMutationRate;
+        public double TranslocatingMutationRate{ 
+            get {return translocatingMutationRate;}
+            set {
+                if(0 <= translocatingMutationRate && translocatingMutationRate <= 1){
+                    translocatingMutationRate = value;
+                }else{
+                    throw new ArgumentException($"{nameof(SingleMutationRate)} must be 0 <= rate <= 1");
+                }
+            }
+        }
+        double translocatingMutationRate;
         ChromosomeEnthumble<T> CurrentEnthumble { get; set;}
+
+        static Random Rand{
+            get {return rand;} 
+        }
+        static Random rand = new Random();
 
         public List<T[]> GeneticManipulation(IList<IChromosome<T>> enthumble){
             var nextGenerationEnthumble = new List<T[]>();
@@ -27,10 +64,17 @@ namespace Optimiser.GeneticAlgorithm
                 );
                 for (int index = 0; index < crossedGenesPair.Length; ++index){
                     var nextGenes = crossedGenesPair[index];
-                    Mutate(nextGenes, enthumble[index].Allele, Mutation.SingleLocus);
+                    if(Rand.NextDouble() < SingleMutationRate){
+                        nextGenes = Mutate(nextGenes, enthumble[index].Allele, Mutation.SingleLocus);
+                    }
+                    if(Rand.NextDouble() < InversingMutationRate){
+                        nextGenes = Mutate(nextGenes, enthumble[index].Allele, Mutation.Inversion);
+                    }
+                    if(Rand.NextDouble() < TranslocatingMutationRate){
+                        nextGenes = Mutate(nextGenes, enthumble[index].Allele, Mutation.Translocation);
+                    }
+                    nextGenerationEnthumble.Add(nextGenes);
                 }
-                nextGenerationEnthumble.Add(crossedGenesPair[0]);
-                nextGenerationEnthumble.Add(crossedGenesPair[1]);
             }
             return nextGenerationEnthumble;
         }
@@ -46,9 +90,9 @@ namespace Optimiser.GeneticAlgorithm
             return crossOver(originalGenesPair);
         }
 
-        public static void Mutate(T[] genes, IChromosome<T>.GetAllele getAllele, Mutation mutation){
+        public static T[] Mutate(T[] genes, IChromosome<T>.GetAllele getAllele, Mutation mutation){
             var mutate = GenesMutatorFactory<T>.Create(mutation);
-            mutate(genes, getAllele);
+            return mutate(genes , getAllele);
         }
 
     }
---
+++ GeneticAlgorithmTests/GATests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Optimiser.GeneticAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optimiser.GeneticAlgorithm.Tests
+{
+    [TestClass()]
+    public class GATests
+    {
+        [TestMethod()]
+        public void GeneticManipulationTest()
+        {
+            var ga = new GA<int>(){
+                Selection = Selection.Roulette,
+                CrossOverMethod = CrossOverMethod.TwoPoints,
+                SingleMutationRate = 0.3,
+                InversingMutationRate = 0.3,
+                TranslocatingMutationRate = 0.3,
+            };
+            var testEnthumble = IntChromosomeTests.CreateSampleIntChromosomes(1000);
+            var nextGenerationEnthumble = ga.GeneticManipulation(testEnthumble);
+            Assert.AreEqual(1000, nextGenerationEnthumble.Count);
+        }
+    }
+}
